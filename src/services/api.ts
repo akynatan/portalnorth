bd3b1858acea255367bdfbc9961ccc161fa52045
Aky Natan <akynatan@outlook.com>
2023-09-11 15:20:18 -0300

    agent http

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import https from 'https';
+
+const agent = new https.Agent({
+  rejectUnauthorized: false, // Isso permite que a solicitação ignore erros SSL
+});
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || '/api',
+  httpAgent: agent,
   validateStatus: (status: number) => {
     if (status >= 200 && status < 300) {
       return true;
